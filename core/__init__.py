727c1f26fd80df1841a942cfe2373ca3ca571221
Smirking Kitsune <36494751+SmirkingKitsune@users.noreply.github.com>
2026-01-22 14:12:54 -0800

    Include DatabaseBusyError and DatabaseQueuedError in core

--- core/__init__.py
+++ core/__init__.py
@@ -1,13 +1,18 @@
 """Core modules for image interrogation framework."""
 
-from .database import InterrogationDatabase
+from .database import InterrogationDatabase, DatabaseBusyError, DatabaseQueuedError
 from .hashing import hash_image_content, get_image_metadata
 from .file_manager import FileManager
 from .base_interrogator import BaseInterrogator
 from .tag_filters import TagFilterSettings
+from .db_queue import DatabaseOperationQueue, QueuedOperation
 
 __all__ = [
     'InterrogationDatabase',
+    'DatabaseBusyError',
+    'DatabaseQueuedError',
+    'DatabaseOperationQueue',
+    'QueuedOperation',
     'hash_image_content',
     'get_image_metadata',
     'FileManager',
