b37ec0bb086f5d3338e58f7a9d244daed4deca06
Smirking Kitsune <36494751+SmirkingKitsune@users.noreply.github.com>
2026-01-22 16:36:30 -0800

    Add CamieInterrogator to __all__ exports

--- interrogators/__init__.py
+++ interrogators/__init__.py
@@ -2,8 +2,10 @@
 
 from .clip_interrogator import CLIPInterrogator
 from .wd_interrogator import WDInterrogator
+from .camie_interrogator import CamieInterrogator 
 
 __all__ = [
     'CLIPInterrogator',
     'WDInterrogator',
+    'CamieInterrogator',
 ]
